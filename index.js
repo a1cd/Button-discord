5559c01cf5ab54656bc57058323314a934b28aaf
a1cd <71281043+a1cd@users.noreply.github.com>
2021-07-01 21:27:55 -0400

    changes

--- index.js
+++ index.js
@@ -4,27 +4,35 @@ const Discord = require('discord.js');
 require('dotenv').config()
 const add = require('./src/add');
 
-let bot = new Discord.Client()
-bot.login(process.env.TOKEN)
-
-let buttons = require('discord-buttons')(bot)
-let {MessageComponent}= require('discord-buttons');
-
-const keyv = new Keyv('mongodb://localhost:27017/'+process.env.DATABASE, {adapter: "mongodb", namespace: "server"})
-keyv.on('error', err => console.error('Keyv connection error:', err));
-
-bot.once("ready", ()=>{
-  bot.on("clickButton", (btn) => {
+module.exports = (bot) => {
+  require('dotenv').config()
+  bot.on("clickButton", async (btn) => {
     /**
      * @type {MessageComponent}
      */
     const button = btn
+    const keyv = new Keyv('mongodb://localhost:27017/'+process.env.DATABASE, {adapter: "mongodb", namespace: button.guild.id})
+    keyv.get(button.message.id)
+    .then(async (info) => {
+      let role = await button.guild.roles.fetch(info.role)
+      await button.clicker.fetch()
+      if (button.id == info.on) {
+        console.log("before add");
+        await button.clicker.member.roles.add(role.id, "button roles")
+        console.log("after add");
+        button.defer()
+      } else if (button.id == info.off) {
+        console.log("before remove");
+        await button.clicker.member.roles.remove(role.id, "button roles")
+        console.log("after remove");
+        button.defer()
+      }
+    })
+    .catch((reason)=>{console.log(reason.stack);console.error("oops", reason)})
   })
 
-  let cmd = new command({name: "btrl", "help": "help message", bot: bot, subcommands: [
+  let cmd = new command({name: "!", "help": "help message", subcommands: [
     new command({name: "add", help: "add a role", commandFunction: add})
   ]})
-
-  bot.on("message", msg => {if (!msg.author.bot) cmd.test(msg.content, msg)
-  console.log(msg);})
-})
+  return cmd
+}
--- src/add.js
+++ src/add.js
@@ -22,22 +22,34 @@ module.exports = async (input, msg, cmd) => {
     .setDisabled(true)
     .setLabel("Epic role!")
     .setStyle("grey")
-    msg.channel.send('Try again, you need to tell me some info first\n```json\n!role add "- getting my attention"\n#channel "- the channel you want it in"\n@role "- the role you want to grant (must already exist)"\nEpic role! "- the text on the role button"```\nexample button:', button)
+    msg.channel.send('Try again, you need to tell me some info first\n```json\n!add "- getting my attention"\n#channel "- the channel you want it in"\n@role "- the role you want to grant (must already exist)"\nEpic role! "- the text on the role button"```\nexample button:', button)
   } else {
     let roles = await msg.mentions.roles.array()
+    if (roles.length == 0){ 
+      msg.reply("that role doesnt exist! (or you need to mention the role for me to understand)")
+    }
     let channels = await msg.mentions.channels.array()
-    let on = new buttons.MessageButton()
+    if (channels.length == 0){ 
+      msg.reply("that channel doesnt exist! (or you need to mention the channel you want!)")
+    }
+    var on = new buttons.MessageButton()
       .setID("on")
-      .setDisabled(true)
-      .setLabel("args[args.length]")
-      .setStyle("grey")
-    let off = on.setID("off")
+      .setStyle("green")
+    var off = new buttons.MessageButton()
+      .setID("off")
+      .setStyle("blurple")
+    on = on.setLabel(args[args.length-1])
     let row = new buttons.MessageActionRow()
-      .addComponent(on)
-      .addComponent(off)
+      .addComponent(on.setEmoji("✅"))
+      .addComponent(off.setEmoji("🇽"))
     let mentionable = roles[0]
-    await mentionable.setMentionable(false, "so as not to wake")
-    await channels[0].send(`hi ${mentionable}`, { buttons: [on, off] })
-    mentionable.setMentionable(mentionable, "so as not to wake")
+    // var thing = mentionable.setMentionable(false, "so as not to wake")
+    // thing.catch((reason) => {log("an error occored", reason)})
+    // await thing
+    let rr = await channels[0].send(`hi ${mentionable}`, row)
+    // var thing = mentionable.setMentionable(mentionable, "so as not to wake")
+    // thing.catch((reason) => {log("an error occored", reason)})
+    // await thing
+    keyv.set(rr.id, {on: "on", off: "off", role: roles[0].id, message: rr.id})
   }
 }
